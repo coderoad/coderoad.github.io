c8c79983e52798607ba07dc9a46291a7b9888281
ShMcK <shawn.j.mckay@gmail.com>
2016-07-02 14:28:26 -0700

    setup front page modals

---
+++ js/modals.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("#atom-coderoad").animatedModal({
+      modalTarget: 'atom-coderoad-demo',
+    });
+    $("#builder-coderoad").animatedModal({
+      modalTarget: 'builder-coderoad-demo',
+    });
+});
