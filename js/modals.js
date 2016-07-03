49841a52c86a11ab352aa7d05c1f37ac2fdc0ab2
ShMcK <shawn.j.mckay@gmail.com>
2016-07-03 16:21:25 -0700

    load gifs on modal open

--- js/modals.js
+++ js/modals.js
@@ -1,8 +1,18 @@
 $(function() {
-    $("#atom-coderoad").animatedModal({
-      modalTarget: 'atom-coderoad-demo',
-    });
-    $("#builder-coderoad").animatedModal({
-      modalTarget: 'builder-coderoad-demo',
-    });
+  $("#atom-coderoad").animatedModal({
+    modalTarget: 'atom-coderoad-demo',
+    animatedIn:'zoomIn',
+    animatedOut:'zoomOut',
+    beforeOpen: function() {
+      $('#atom-coderoad-demo .modal-content img')
+      .attr("src", "images/demos/atom-coderoad.gif");
+    }
+  });
+  $("#builder-coderoad").animatedModal({
+    modalTarget: 'builder-coderoad-demo',
+    beforeOpen: function() {
+      $('#builder-coderoad-demo .modal-content img')
+        .attr("src", "images/demos/builder-demo.gif");
+    }
+  });
 });
