6d3e71ca72ba659646c48013bde50e096cbd1fbe
ShMcK <shawn.j.mckay@gmail.com>
2016-07-04 12:29:47 -0700

    refactor includes, smooth scroll on page change

--- js/modals.js
+++ js/modals.js
@@ -33,4 +33,36 @@ $(function() {
     preventDefault: true
   });
 
+
+  const scrollnow = function(e) {
+    var target;
+    if (e) {
+      e.preventDefault();
+      target = this.hash;
+    } else {
+      target = location.hash;
+    }
+    $.smoothScroll({
+      offset: -70,
+      easing: 'swing',
+      speed: 400,
+      autoCoefficient: 2,
+      preventDefault: true,
+      scrollTarget: target
+    });
+  };
+
+  if (location.hash) {
+    $('html, body').scrollTop(0).show();
+      scrollnow();
+    }
+
+  $('a[href*="/"][href*=#]').each(function(){
+    if (this.pathname.replace(/^\//,'') == location.pathname.replace(/^\//,'') && this.hostname == location.hostname) {
+      $(this).attr("href", this.hash);
+    }
+  });
+
+  $('a[href^=#]:not([href=#])').click(scrollnow);
+
 });
