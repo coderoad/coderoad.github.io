b7e2fbf992ee7659fcbccde7da1bf42b9d66118d
ShMcK <shawn.j.mckay@gmail.com>
2016-07-04 11:44:45 -0700

    add smooth scroll

--- js/modals.js
+++ js/modals.js
@@ -24,17 +24,13 @@ $(function() {
   });
 
   // smooth scroll
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
+  $('a.smooth').smoothScroll({
+    offset: -70,
+    direction: 'top',
+    easing: 'swing',
+    speed: 400,
+    autoCoefficient: 2,
+    preventDefault: true
   });
 
 });
